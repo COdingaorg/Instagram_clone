e83922d4a0c9b979998a8296f2552261f999d49c
Caleb Odinga <calemasanga@gmail.com>
2021-07-11 00:44:24 +0300

    adds pillow and media configuration to settings

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 from gallery import views
+from django.conf import settings
 
 urlpatterns = [
   url(r'^$', views.index, name = 'home'),
   url(r'^register_user/', views.register_user, name = 'register_user'),
 ]
+if settings.DEBUG:
+  urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

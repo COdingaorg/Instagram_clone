083bff6f2fb89a20d31d0e2f2e6e3b6b3f371e52
Caleb Odinga <calemasanga@gmail.com>
2021-07-11 00:49:41 +0300

    adds user profile model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+  photo_path = models.ImageField(upload_to = 'gallery/')
+  bio = models.CharField(200)
+  

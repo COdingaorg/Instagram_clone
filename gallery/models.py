047156b24672f46da5a47d833932a8750cc2a1cc
Caleb Odinga <calemasanga@gmail.com>
2021-07-11 07:42:57 +0300

    adds followers and follow foreign keys to user profile model

--- gallery/models.py
+++ gallery/models.py
@@ -4,10 +4,18 @@ from django.db.models.base import Model
 from django.db.models.deletion import CASCADE, RESTRICT, SET_DEFAULT, SET_NULL
 
 # Create your models here.
+class FollowChain(models.Model):
+  follow = models.ForeignKey(User, on_delete=CASCADE)
+
+class Follower(models.Model):
+  followers = models.ForeignKey(User, on_delete=CASCADE)
+
 class UserProfile(models.Model):
   photo_path = models.ImageField(upload_to = 'gallery/')
   bio = models.CharField(max_length=200)
   user = models.ForeignKey(User, on_delete=CASCADE)
+  follows = models.ForeignKey(FollowChain, on_delete=CASCADE, null=True)
+  followers = models.ForeignKey(Follower, on_delete=CASCADE, null=True)
 
 class PostLikes(models.Model):
   likes = models.IntegerField(auto_created=True)
@@ -24,12 +32,3 @@ class ImagePost(models.Model):
   profile = models.ForeignKey(UserProfile, on_delete=CASCADE)
   likes = models.ForeignKey(PostLikes, on_delete=CASCADE)
   comments = models.ForeignKey(PostComment, on_delete=CASCADE)
-
-class FollowChain(models.Model):
-  follow = models.ForeignKey(User, on_delete=CASCADE)
-
-class Follower(models.Model):
-  followers = models.ForeignKey(User, on_delete=CASCADE)
-
-
-

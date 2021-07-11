1f215454fb723a127bb701b683a4dad1ca33f142
Caleb Odinga <calemasanga@gmail.com>
2021-07-11 07:04:31 +0300

    adds user profile model and image post model

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE, SET_NULL
 
 # Create your models here.
 class UserProfile(models.Model):
   photo_path = models.ImageField(upload_to = 'gallery/')
-  bio = models.CharField(200)
-  
+  bio = models.CharField(max_length=200)
+  user = models.ForeignKey(User, on_delete=CASCADE)
+
+class ImagePost(models.Model):
+  image = models.ImageField(upload_to = 'posts/')
+  image_name = models.CharField(max_length=100)
+  image_caption = models.TextField(max_length=250)
+  profile = models.ForeignKey(UserProfile, on_delete=CASCADE)

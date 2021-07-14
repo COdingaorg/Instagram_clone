41b5358e9416225b4ee744e421b8832d5a0bc69f
Caleb Odinga <calemasanga@gmail.com>
2021-07-14 10:36:10 +0300

    adds email configurations in email.py file , email send view function

---
+++ gallery/emails.py
@@ -0,0 +1,17 @@
+from pinstagram import settings
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
+
+#sending a welcome email
+def send_new_user_email(request):
+  #creating subject and sender
+  subject = 'Welcome to Pinstagram'
+  sender = settings.EMAIL_HOST_USER
+
+  #context variables
+  text_context = render_to_string('email/welcome.txt', {'name':name})
+  html_context = render_to_string('email/welcome.html', {'name':name})
+
+  msg = EmailMultiAlternatives(subject, text_context, sender, [receiver])
+  msg.attach_alternative(html_context, 'text/html')
+  msg.send()
--- gallery/views.py
+++ gallery/views.py
@@ -6,9 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import FollowChain, Follower, ImagePost, PostComment, PostLikes, UserProfile, User
 import datetime as dt
-from pinstagram import settings
-from django.template.loader import render_to_string
-from django.core.mail import EmailMultiAlternatives
+
 
 # Create your views here.
 #register user view function
@@ -329,18 +327,3 @@ def follow_followed(request):
 
   else:
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-
-
-#sending a welcome email
-def send_new_user_email(request):
-  #creating subject and sender
-  subject = 'Welcome to Pinstagram'
-  sender = settings.EMAIL_HOST_USER
-
-  #context variables
-  text_context = render_to_string('email/welcome.txt', {'name':name})
-  html_context = render_to_string('email/welcome.html', {'name':name})
-
-  msg = EmailMultiAlternatives(subject, text_context, sender, [receiver])
-  msg.attach_alternative(html_context, 'text/html')
-  msg.send()

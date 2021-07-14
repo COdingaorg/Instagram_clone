e1523bdabd36ca84511ecb59ca8e6c98e4a17635
Caleb Odinga <calemasanga@gmail.com>
2021-07-14 14:11:35 +0300

    add requirements file

--- pinstagram/settings.py
+++ pinstagram/settings.py
@@ -29,7 +29,7 @@ LOGOUT_REDIRECT_URL = 'login'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
